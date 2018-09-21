ff99021d6d5f42cb6c637d7510444366fc606767
Sawyer Warden <sawyer.warden.com>
2018-09-20 23:28:14 -0400

    updating

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -4,10 +4,53 @@ defmodule Practice.Calc do
     num
   end
 
+# !!!!!!!! change n to i
+
+
+##new_l arg should be int acc, accumulate evaluations in n and recur
+  def eval(l, acc, n) do
+    #n = 0
+    el1 = Enum.at(l, 0)
+    el2 = Enum.at(l, 1)
+    if length(l) <= 2 or el2 == nil do
+      n = n + [parse_float(el1)]
+      #new_l ++ n
+      #acc = ""
+      #r_val = Enum.reduce(new_l, acc, fn(x, acc)-> x<>acc end)
+    else
+      Enum.find(l, fn(el)->
+        if (match?("*", el) or match?("/", el)) do
+          i = i + 2
+          if el2 == "*" do
+            n = n + [parse_float(el1) * parse_float(Enum.at(l, i))]
+          end
+          if el2 == "/" do
+            n = n + [parse_float(el1) / parse_float(Enum.at(l, i))]
+          end
+          new_l = [n] ++ Enum.slice(l, i+2, length(l))
+          eval(l, new_l, i)
+        end
+      end)
+      #if no * or / in l, but we still have 3 or more els in l, evaluate
+      if el2 == "+" do
+        i = i + 2
+        n = n + [parse_float(el1) + parse_float(Enum.at(l, i))]
+      end
+      if el2 == "-" do
+        i = i + 2
+        n = n + [parse_float(el1) - parse_float(Enum.at(l, i))]
+      end
+      new_l = n ++ Enum.slice(l, i, length(l))
+      eval(l, new_l, i)
+    end
+  end
+
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
     expr
+    |>String.codepoints()
+    |>eval(0,0)
     #|> String.split(~r/\s+/)
     #|> hd
     #|> parse_float
